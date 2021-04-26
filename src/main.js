4481fda90119313d7b4e38bcf67e273839ccf2a6
Min Chang <minchangmhc@gmail.com>
2021-04-26 14:47:05 -0700

    Add basic HTML format
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com
    >
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>
    Co-authored-by: Carlos Mendez <carlosmendez86@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ParkFinder from './src/js/finder.js';
 console.log($);
+console.log(ParkFinder);
+
+$('#submit').on('click', (event) => {
+  event.preventDefault();
+  let zipcode = $('#zipcode').val();
+
+});
